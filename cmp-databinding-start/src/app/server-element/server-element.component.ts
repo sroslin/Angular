f04710f85906334d0afc17a16703aea7eafe0009
sroslin <blades2@bellsouth.net>
2018-07-06 15:03:03 -0400

    finishing with ContentChild example

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -8,7 +8,10 @@ import {
   AfterContentChecked,
   AfterViewChecked,
   AfterViewInit,
-  OnDestroy
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -27,6 +30,8 @@ export class ServerElementComponent implements
   OnDestroy {
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
   constructor() {
     console.log('constructor called!');
    }
@@ -38,6 +43,8 @@ export class ServerElementComponent implements
 
   ngOnInit() {
     console.log('ngOnInit called!');
+    console.log('Text Content:' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph:' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -46,6 +53,7 @@ export class ServerElementComponent implements
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph:' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
@@ -54,6 +62,7 @@ export class ServerElementComponent implements
 
   ngAfterViewInit() {
     console.log('ngAfterViewInit called!');
+    console.log('Text Content:' + this.header.nativeElement.textContent);
   }
 
   ngAfterViewChecked() {

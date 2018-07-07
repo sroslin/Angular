7f7a7583586c422642559e007dd0b54ecd551c43
sroslin <blades2@bellsouth.net>
2018-07-07 08:27:09 -0400

    Added function to display shopping or recipe list based on button click

--- Project1/src/app/app.component.ts
+++ Project1/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  loadedList = 'recipe';
+
+  displayList(chooseList: string) {
+      this.loadedList = chooseList;
+  }
 }
--- Project1/src/app/header/header.component.ts
+++ Project1/src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() selectionEmitter = new EventEmitter<string>();
 
+    onSelect(selection: string) {
+        this.selectionEmitter.emit(selection);
+    }
 }

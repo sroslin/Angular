722be4a693e75c42dc859e9259f07e89f07cca2e
sroslin <blades2@bellsouth.net>
2018-07-09 15:44:10 -0400

    created custom structural directive example

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BeterHighlightDirective } from './better-highlight/beter-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BeterHighlightDirective
+    BeterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,

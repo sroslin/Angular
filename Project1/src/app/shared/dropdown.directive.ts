40b32a1708fa03f8603460f14e75477436f873bf
sroslin <blades2@bellsouth.net>
2018-07-09 16:22:03 -0400

    Created dropdown directive, just have to implement now

--- Project1/src/app/shared/dropdown.directive.ts
+++ Project1/src/app/shared/dropdown.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from '../../../node_modules/@angular/core';
+import { Directive, HostListener, Input, HostBinding } from '../../../node_modules/@angular/core';
+import { Button } from '../../../node_modules/@types/selenium-webdriver';
 
 @Directive({
   selector: '[appDropdown]'
 })
+
 export class DropdownDirective {
-  
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }

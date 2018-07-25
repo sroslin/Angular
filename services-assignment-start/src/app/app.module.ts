109686026524339f8131178084f560602b9d3d20
Sean <blades2@bellsouth.net>
2018-07-25 13:13:26 -0400

    Implemented a users service to change users from active to inactive/vice versa

--- services-assignment-start/src/app/app.module.ts
+++ services-assignment-start/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { UsersService } from './users.service';
+import { CounterService } from './counter.service';
 
 @NgModule({
   declarations: [
